c087ed280407c99bf68a528cc487fa0f4050aeee
gamine <48608531+gamin-yojo@users.noreply.github.com>
2019-07-15 16:59:05 +0900

    jQueryを記述

---
+++ sync/static/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+$(".write-btn").on("click", function() {
+	$(".write-screen").addClass("active");
+});
+$(".cancel-btn").on("click", function() {
+	$(".write-screen").removeClass("active");
+});
